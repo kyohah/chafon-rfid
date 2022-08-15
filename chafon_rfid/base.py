485e5192287660f1d146ba2ba0250f99732a0995
kyohah <kyohah@gmail.com>
2022-08-15 23:05:24 +0900

    add UHFReader299M

--- chafon_rfid/base.py
+++ chafon_rfid/base.py
@@ -77,14 +77,15 @@ class ReaderFrequencyBand(Enum):
 
 class ReaderType(Enum):
 
-    RRU9803M      = 0x03 # CF-RU5102 (desktop USB reader/writer, as specified)
-    RRU9803M_1    = 0x08 # CF-RU5102 (desktop USB reader/writer, actual)
-    UHFReader18   = 0x09
-    UHFReader288M = 0x0c
-    UHFReader86   = 0x0f # CF-MU903/CF-MU904 (as documented)
-    UHFReader86_1 = 0x10 # CF-MU903/CF-MU904 (actual)
-    RRU9883M      = 0x16 # CF-MU902
+    RRU9803M       = 0x03 # CF-RU5102 (desktop USB reader/writer, as specified)
+    RRU9803M_1     = 0x08 # CF-RU5102 (desktop USB reader/writer, actual)
+    UHFReader18    = 0x09
+    UHFReader288M  = 0x0c
+    UHFReader86    = 0x0f # CF-MU903/CF-MU904 (as documented)
+    UHFReader86_1  = 0x10 # CF-MU903/CF-MU904 (actual)
+    RRU9883M       = 0x16 # CF-MU902
     UHFReader288MP = 0x20 # CF-MU804
+    UHFReader299M  = 0x65 # CF-MU616
 
 
 class ReaderInfoFrame(ReaderResponseFrame):
